054889eb662f6e2278bc55f65dbd3b9bb88f17c7
Freddy Garcia <90714730+FreddyGar@users.noreply.github.com>
2023-11-13 14:48:52 -0500

    Add files via upload

---
+++ Clase_práctica3/productos.php
@@ -0,0 +1,25 @@
+<?php
+$productos = array(
+    array(
+        'id' => 1,
+        'nombre' => 'MacBook Desktop',
+        'descripcion' => 'CPU de 8 núcleos con 4 núcleos de rendimiento.Motor multimedia
+        H.264, HEVC, ProRes y ProRes RAW con aceleración por hardware',
+        'precio' => 750.99
+    ),
+    array(
+        'id' => 2,
+        'nombre' => 'All in One Lenovo',
+        'descripcion' => 'Memoria Ram 8GB DDR4-2400 MHz (1 x 8GB) ranuras totales 2 SODIMM.
+        – Disco duro estado sólido PCIe® NVMe™ M.2 de 256 GB.',
+        'precio' => 650.99
+    ),
+    array(
+        'id' => 3,
+        'nombre' => 'Mainboard Asus',
+        'descripcion' => 'Mother Board Asus Prime A520m-a Ii/csm Ddr4 Am4 Hdmi Vga Dp Combo Intel Core I5-10400 + 8gb + Ssd 480 + H470 Mainboard.',
+        'precio' => 180.99
+    )
+    // Puedes agregar más productos según sea necesario.
+);
+?>
