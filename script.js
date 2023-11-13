eac680c60ed7c766472c052aee3effaf6c081bad
Freddy Garcia <90714730+FreddyGar@users.noreply.github.com>
2023-11-13 15:14:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 // script.js
 function agregarAlCarrito(idProducto) {
-    // Aquí puedes agregar la lógica para agregar el producto al carrito usando JavaScript.
 
-    // Por ejemplo, podrías cambiar el texto del botón a "Agregado al carrito".
     var boton = document.querySelector('button[data-id="' + idProducto + '"]');
     if (boton) {
         boton.textContent = 'Agregado al carrito';
-        boton.disabled = true; // Deshabilita el botón después de agregar al carrito
+        boton.disabled = true; 
     }
-
-    // Puedes realizar otras acciones, como enviar datos al servidor, actualizar el carrito, etc.
     alert('Producto agregado al carrito: ' + idProducto);
 }
